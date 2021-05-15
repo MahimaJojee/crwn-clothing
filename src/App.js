cc8469d54a8e6fd19f71c2b1995177d7e0b49e68
MahimaJojee <mahima.jojee@gmail.com>
2021-05-15 19:35:04 +0530

    Added shop page and related compoenents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { Route } from "react-router-dom";
 
+const HatsPage = (props) =>{
+  console.log(props)
+  return (
+    <div>
+      <h1>Hey you are at the hats page!</h1>
+      {/* {console.log(`${props.match.url}and${props.linkUrl}`)} */}
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/shops" component={ShopPage}/>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import './homepage.style.scss';
 import Directory from '../../components/directory/directory.component.jsx'
 
-const HomePage = () => (
+const HomePage = (props) => {
+  console.log(props);
+  return(
   <div className="homepage">
     <Directory/>
   </div>
-);
+  );
+};
 
 export default HomePage;
